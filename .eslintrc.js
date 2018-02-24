f1cc78c6771c1e123ce85f2f45236e533e5d2985
Rhian van Esch <rhian.vanesch@gmail.com>
2018-02-24 15:41:59 +0000

    Remove rules from ESLint config that are covered by Prettier config

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,18 +4,12 @@ module.exports = {
     commonjs: true,
     es6: true
   },
-  extends: [
-    "eslint:recommended",
-    "plugin:prettier/recommended"
-  ],
+  extends: ["eslint:recommended", "plugin:prettier/recommended"],
   parserOptions: {
     sourceType: "module"
   },
   rules: {
-    indent: ["error", 2],
     "linebreak-style": ["error", "unix"],
-    quotes: ["error", "double"],
-    semi: ["error", "always"],
     "compat/compat": "warn"
   },
   plugins: ["compat"]
